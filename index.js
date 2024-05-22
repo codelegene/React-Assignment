26e6a2bbbe274ea37b62f796689c17a4137222fc
codelegene <126109035+codelegene@users.noreply.github.com>
2024-05-22 16:35:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.use(express.static('public'))
+
+const root = require('path').join(__dirname, '../client', 'dist')
+app.use(express.static(root))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root })
+})
+
+app.get('/test', (req, res) => {
+    res.send('Server is operational')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
+// image is random-image.webp
